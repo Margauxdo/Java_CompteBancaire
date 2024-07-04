b66fa977cdf9d3373e5e56eb2a5e4ead965406d7
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-04 14:42:45 +0200

    modif repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,17 @@
 package org.example;
 
-import org.example.repository.RepositoryClient;
-import org.example.repository.RepositoryCompteBancaire;
-import org.example.repository.RepositoryOperation;
+import org.example.repository.*;
 
 public class Main {
     public static void main(String[] args) {
 
         RepositoryClient client = new RepositoryClient();
 
-        RepositoryCompteBancaire compteBancaire = new RepositoryCompteBancaire();
+        RepositoryCompteCourant courant = new RepositoryCompteCourant();
+
+        RepositoryCompteEpargne epargne = new RepositoryCompteEpargne();
+
+        RepositoryComptePayant payant = new RepositoryComptePayant();
 
         RepositoryOperation operation = new RepositoryOperation();
     }
